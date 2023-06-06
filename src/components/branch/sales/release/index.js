c3621a42ae672a4dee371cc26bbaf97e2cd736be
Developer <developer.krishnasoftweb@gmail.com>
2023-06-06 10:13:43 +0530

    Bug fixes

--- src/components/branch/sales/release/index.js
+++ src/components/branch/sales/release/index.js
@@ -32,7 +32,6 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import CloseIcon from '@mui/icons-material/Close';
 import SaveIcon from '@mui/icons-material/Save';
 import { useState, useEffect } from 'react';
-import { createPortal } from 'react-dom';
 import { useSelector } from 'react-redux';
 import Iconify from '../../../iconify';
 import { getReleaseByCustomerId, createRelease, deleteReleaseById } from '../../../../apis/branch/release';
@@ -70,6 +69,91 @@ function Release({ setNotify, selectedUser, selectedRelease, setSelectedRelease 
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [width, setWindowWidth] = useState(0);
   const modalRoot = document.getElementById('root-modal');
+  // Form validation
+  const schema = Yup.object({
+    weight: Yup.string().required('Weight is required'),
+    pledgeAmount: Yup.string().required('Pledge amount is required'),
+    payableAmount: Yup.string().required('Payable amount is required'),
+    paymentType: Yup.string().required('Payment type is required'),
+    pledgedDate: Yup.string().required('Pledged date is required'),
+    pledgeId: Yup.string().required('Pledge id is required'),
+    pledgedIn: Yup.string().required('Pledged in is required'),
+    pledgedBranch: Yup.string().required('Pledged branch is required'),
+    releaseDate: Yup.string().required('Release date is required'),
+    comments: Yup.string().required('comments is required'),
+  });
+
+  const { handleSubmit, handleChange, handleBlur, values, setValues, touched, errors } = useFormik({
+    initialValues: {
+      customer: selectedUser?._id,
+      weight: '',
+      pledgeAmount: '',
+      payableAmount: '',
+      paymentType: '',
+      bank: selectedBank?._id,
+      pledgedDate: moment(),
+      pledgeId: '',
+      pledgedIn: '',
+      pledgedBranch: '',
+      releaseDate: moment(),
+      comments: '',
+      releaseDocument: {},
+      status: 'active',
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      if (values.paymentType === 'bank' && !selectedBank) {
+        setNotify({
+          open: true,
+          message: 'Please select bank',
+          severity: 'info',
+        });
+        // alert('Please select bank');
+        return;
+      }
+      const payload = {
+        customer: values.customer,
+        branch: branch?._id,
+        weight: values.weight,
+        pledgeAmount: values.pledgeAmount,
+        payableAmount: values.payableAmount,
+        paymentType: values.paymentType,
+        bank: values.bank,
+        pledgedDate: values.pledgedDate,
+        pledgeId: values.pledgeId,
+        pledgedIn: values.pledgedIn,
+        pledgedBranch: values.pledgedBranch,
+        releaseDate: values.releaseDate,
+        comments: values.comments,
+        status: values.status,
+      };
+      createRelease(payload).then((data) => {
+        if (data.status === false) {
+          setNotify({
+            open: true,
+            message: 'Release not created',
+            severity: 'error',
+          });
+        } else {
+          getReleaseByCustomerId(selectedUser._id).then((data) => {
+            setData(data.data);
+          });
+          const formData = new FormData();
+          formData.append('uploadId', data.data.fileUpload.uploadId);
+          formData.append('uploadName', data.data.fileUpload.uploadName);
+          formData.append('uploadType', 'proof');
+          formData.append('uploadedFile', values.releaseDocument);
+          createFile(formData);
+          setReleaseModal(false);
+          setNotify({
+            open: true,
+            message: 'Release created',
+            severity: 'success',
+          });
+        }
+      });
+    },
+  });
 
   useEffect(() => {
     getBranchByBranchId({ branchId: auth.user.username }).then((data) => {
@@ -129,85 +213,101 @@ function Release({ setNotify, selectedUser, selectedRelease, setSelectedRelease 
     });
   };
 
-  const CreateReleaseModal = () => {
-    // Form validation
-    const schema = Yup.object({
-      weight: Yup.string().required('Weight is required'),
-      pledgeAmount: Yup.string().required('Pledge amount is required'),
-      payableAmount: Yup.string().required('Payable amount is required'),
-      paymentType: Yup.string().required('Payment type is required'),
-      pledgedDate: Yup.string().required('Pledged date is required'),
-      pledgeId: Yup.string().required('Pledge id is required'),
-      pledgedIn: Yup.string().required('Pledged in is required'),
-      pledgedBranch: Yup.string().required('Pledged branch is required'),
-      releaseDate: Yup.string().required('Release date is required'),
-      comments: Yup.string().required('comments is required'),
-    });
+  return (
+    <>
+      <Grid item xs={12}>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mt={2} mb={3}>
+          <Typography variant="h4" gutterBottom>
+            Customer Release
+          </Typography>
+          <Button
+            variant="contained"
+            startIcon={<Iconify icon="eva:plus-fill" />}
+            onClick={() => setReleaseModal(true)}
+          >
+            New Release
+          </Button>
+        </Stack>
+        <Scrollbar>
+          <TableContainer sx={{ minWidth: 800 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left" />
+                  <TableCell align="left">Pledge Id</TableCell>
+                  <TableCell align="left">Pledged In</TableCell>
+                  <TableCell align="left">Weight (Grams)</TableCell>
+                  <TableCell align="left">Pledge amount</TableCell>
+                  <TableCell align="left">Pledged date</TableCell>
+                  <TableCell align="left">Payable amount</TableCell>
+                  <TableCell align="left">Payment Type</TableCell>
+                  <TableCell align="left">Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)?.map((e) => (
+                  <TableRow hover key={e._id} tabIndex={-1}>
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        checked={selectedRelease.find((v) => v._id === e._id)}
+                        onChange={() => handleSelect(e)}
+                      />
+                    </TableCell>
+                    <TableCell align="left">{e.pledgeId}</TableCell>
+                    <TableCell align="left">{sentenceCase(e.pledgedIn)}</TableCell>
+                    <TableCell align="left">{e.weight}</TableCell>
+                    <TableCell align="left">{e.pledgeAmount}</TableCell>
+                    <TableCell align="left">{moment(e.pledgedDate).format('YYYY-MM-DD')}</TableCell>
+                    <TableCell align="left">{e.payableAmount}</TableCell>
+                    <TableCell align="left">{sentenceCase(e.paymentType)}</TableCell>
+                    <TableCell align="left">
+                      <Button
+                        variant="contained"
+                        startIcon={<DeleteIcon />}
+                        onClick={() => {
+                          setOpenId(e._id);
+                          handleOpenDeleteModal();
+                        }}
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 53 * emptyRows }}>
+                    <TableCell colSpan={9} />
+                  </TableRow>
+                )}
+                {data.length === 0 && (
+                  <TableRow>
+                    <TableCell align="center" colSpan={9} sx={{ py: 3 }}>
+                      <Paper
+                        sx={{
+                          textAlign: 'center',
+                        }}
+                      >
+                        <Typography paragraph>No data in table</Typography>
+                      </Paper>
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
 
-    const { handleSubmit, handleChange, handleBlur, values, setValues, touched, errors } = useFormik({
-      initialValues: {
-        customer: selectedUser?._id,
-        weight: '',
-        pledgeAmount: '',
-        payableAmount: '',
-        paymentType: '',
-        bank: selectedBank?._id,
-        pledgedDate: moment(),
-        pledgeId: '',
-        pledgedIn: '',
-        pledgedBranch: '',
-        releaseDate: moment(),
-        comments: '',
-        releaseDocument: {},
-        status: 'active',
-      },
-      validationSchema: schema,
-      onSubmit: (values) => {
-        const payload = {
-          customer: values.customer,
-          branch: branch?._id,
-          weight: values.weight,
-          pledgeAmount: values.pledgeAmount,
-          payableAmount: values.payableAmount,
-          paymentType: values.paymentType,
-          bank: values.bank,
-          pledgedDate: values.pledgedDate,
-          pledgeId: values.pledgeId,
-          pledgedIn: values.pledgedIn,
-          pledgedBranch: values.pledgedBranch,
-          releaseDate: values.releaseDate,
-          comments: values.comments,
-          status: values.status,
-        };
-        createRelease(payload).then((data) => {
-          if (data.status === false) {
-            setNotify({
-              open: true,
-              message: 'Release not created',
-              severity: 'error',
-            });
-          } else {
-            getReleaseByCustomerId(selectedUser._id).then((data) => {
-              setData(data.data);
-            });
-            const formData = new FormData();
-            formData.append('uploadId', data.data.fileUpload.uploadId);
-            formData.append('uploadName', data.data.fileUpload.uploadName);
-            formData.append('uploadType', 'proof');
-            formData.append('uploadedFile', values.releaseDocument);
-            createFile(formData);
-            setReleaseModal(false);
-            setNotify({
-              open: true,
-              message: 'Release created',
-              severity: 'success',
-            });
-          }
-        });
-      },
-    });
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={data.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Scrollbar>
+      </Grid>
 
-    return createPortal(
       <Modal
         open={releaseModal}
         onClose={() => setReleaseModal(false)}
@@ -223,21 +323,7 @@ function Release({ setNotify, selectedUser, selectedRelease, setSelectedRelease 
               onClick={() => setReleaseModal(false)}
             />
           </Typography>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (values.paymentType === 'bank' && !selectedBank) {
-                setNotify({
-                  open: true,
-                  message: 'Please select bank',
-                  severity: 'info',
-                });
-              } else {
-                handleSubmit(e);
-              }
-            }}
-            autoComplete="off"
-          >
+          <form onSubmit={handleSubmit} autoComplete="off">
             <Grid container spacing={3}>
               <Grid item xs={12} md={4}>
                 <TextField
@@ -406,107 +492,7 @@ function Release({ setNotify, selectedUser, selectedRelease, setSelectedRelease 
             </Grid>
           </form>
         </Box>
-      </Modal>,
-      modalRoot
-    );
-  };
-
-  return (
-    <>
-      <Grid item xs={12}>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mt={2} mb={3}>
-          <Typography variant="h4" gutterBottom>
-            Customer Release
-          </Typography>
-          <Button
-            variant="contained"
-            startIcon={<Iconify icon="eva:plus-fill" />}
-            onClick={() => setReleaseModal(true)}
-          >
-            New Release
-          </Button>
-        </Stack>
-        <Scrollbar>
-          <TableContainer sx={{ minWidth: 800 }}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell align="left" />
-                  <TableCell align="left">Pledge Id</TableCell>
-                  <TableCell align="left">Pledged In</TableCell>
-                  <TableCell align="left">Weight (Grams)</TableCell>
-                  <TableCell align="left">Pledge amount</TableCell>
-                  <TableCell align="left">Pledged date</TableCell>
-                  <TableCell align="left">Payable amount</TableCell>
-                  <TableCell align="left">Payment Type</TableCell>
-                  <TableCell align="left">Action</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)?.map((e) => (
-                  <TableRow hover key={e._id} tabIndex={-1}>
-                    <TableCell padding="checkbox">
-                      <Checkbox
-                        checked={selectedRelease.find((v) => v._id === e._id)}
-                        onChange={() => handleSelect(e)}
-                      />
-                    </TableCell>
-                    <TableCell align="left">{e.pledgeId}</TableCell>
-                    <TableCell align="left">{sentenceCase(e.pledgedIn)}</TableCell>
-                    <TableCell align="left">{e.weight}</TableCell>
-                    <TableCell align="left">{e.pledgeAmount}</TableCell>
-                    <TableCell align="left">{moment(e.pledgedDate).format('YYYY-MM-DD')}</TableCell>
-                    <TableCell align="left">{e.payableAmount}</TableCell>
-                    <TableCell align="left">{sentenceCase(e.paymentType)}</TableCell>
-                    <TableCell align="left">
-                      <Button
-                        variant="contained"
-                        startIcon={<DeleteIcon />}
-                        onClick={() => {
-                          setOpenId(e._id);
-                          handleOpenDeleteModal();
-                        }}
-                      >
-                        Delete
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-                {emptyRows > 0 && (
-                  <TableRow style={{ height: 53 * emptyRows }}>
-                    <TableCell colSpan={9} />
-                  </TableRow>
-                )}
-                {data.length === 0 && (
-                  <TableRow>
-                    <TableCell align="center" colSpan={9} sx={{ py: 3 }}>
-                      <Paper
-                        sx={{
-                          textAlign: 'center',
-                        }}
-                      >
-                        <Typography paragraph>No data in table</Typography>
-                      </Paper>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            component="div"
-            count={data.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Scrollbar>
-      </Grid>
-
-      <CreateReleaseModal />
+      </Modal>
 
       <Modal
         open={openDeleteModal}
